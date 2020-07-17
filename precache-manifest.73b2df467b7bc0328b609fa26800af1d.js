151918981012c2595e120677821df2f02dda00b8
Paeddel <paddystaf@gmail.com>
2020-07-17 13:25:02 +0200

    Updates

---
+++ precache-manifest.73b2df467b7bc0328b609fa26800af1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d05cdfc3c8f02841c8260edcefed6b7",
+    "url": "/github-hello-world/index.html"
+  },
+  {
+    "revision": "a9e45804b676b382b8e7",
+    "url": "/github-hello-world/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/github-hello-world/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-hello-world/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e45804b676b382b8e7",
+    "url": "/github-hello-world/static/js/main.735829ba.chunk.js"
+  },
+  {
+    "revision": "6451814ed822907d6f27",
+    "url": "/github-hello-world/static/js/runtime-main.399f5076.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
